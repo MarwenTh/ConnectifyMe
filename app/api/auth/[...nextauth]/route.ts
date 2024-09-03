ec44d0ad5ebdc1aac07d4153d123bfafa6de6f2d
Marwen Tahouri <marwentahouri2@gmail.com>
2024-09-03 16:42:48 +0100

    nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,8 +9,13 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
     // ...add more providers here
   ],
 };
 
-export default NextAuth(authOptions);
+export const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { BottomGradient } from "../ui/BottomGradient";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
 
 type Props = {};
 
@@ -21,6 +22,7 @@ const Login = (props: Props) => {
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             type="submit"
+            onClick={() => signIn("github")}
           >
             <FaGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
@@ -31,6 +33,7 @@ const Login = (props: Props) => {
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             type="submit"
+            onClick={() => signIn("google")}
           >
             <FaGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
